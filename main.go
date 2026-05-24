6ad102fba4517c48f90e5e70424b0c0e1995e3a7
Ursula Dalja <urshudalaj@users.noreply.github.com>
2026-05-25 01:06:02 +0800

    fix: complete truncated main() function body

--- main.go
+++ main.go
@@ -87,7 +87,6 @@ func main() {
 	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
 
 	http.Handle(*metricsPath, handler.NewHandler(!*disableExporterMetrics, *maxRequests, logger))
-
 	if *metricsPath != "/" {
 		landingConfig := web.LandingConfig{
 			Name:        "Node Exporter",
